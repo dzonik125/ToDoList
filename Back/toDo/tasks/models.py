e37f703aaee4baea15924e98af110306eee6fc35
Nikola Kolarov <dzonik125@gmail.com>
2022-10-01 16:37:51 +0200

    Conected mongoDB with backend and made get All tasks function

--- Back/toDo/tasks/models.py
+++ Back/toDo/tasks/models.py
@@ -5,5 +5,8 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    finishDate = models.DateTimeField(auto_now_add=True)
-    createdDate = models.DateTimeField
+    finishDate = models.DateTimeField()
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- Back/toDo/tasks/views.py
+++ Back/toDo/tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Task
+from django.core import serializers
+from django.http import HttpResponse
+
 
 # Create your views here.
+def get_all_tasks(request):
+    qs = Task.objects.all()
+    qs_json = serializers.serialize('json', qs)
+    return HttpResponse(qs_json, content_type='application/json')
